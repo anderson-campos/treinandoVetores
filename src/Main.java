583ac0cd84ee83ef110cb64e454c1f1664f255c6
Anderson Campos <andersonbcamposbr@gmail.com>
2024-10-30 12:09:09 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[] numeros = new int[5];
+
+        numeros[0] = 1;
+        numeros[1] = 2;
+        numeros[2] = 3;
+        numeros[3] = 4;
+        numeros[4] = 5;
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("A posição " + i + " temos o numero: " + numeros[i]);
+        }
+    }
+}
